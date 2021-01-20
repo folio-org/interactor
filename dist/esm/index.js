630a06b51e56252a22367804fd23fa09c9dd3272
Michal Kuklis <michal.kuklis@gmail.com>
2021-01-20 15:19:52 -0500

    Adjust build

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -898,7 +898,7 @@ function selectable(selector) {
 
 function focus(selector) {
   return find.call(this, selector)['do'](function($node) {
-    $node.dispatchEvent(new Event('focus', {
+    $node.dispatchEvent(new Event('focusin', {
       bubbles: true,
       cancelable: true
     }));
@@ -912,7 +912,7 @@ function focusable(selector) {
 
 function blur(selector) {
   return find.call(this, selector)['do'](function($node) {
-    $node.dispatchEvent(new Event('focus', {
+    $node.dispatchEvent(new Event('focusout', {
       bubbles: true,
       cancelable: true
     }));
